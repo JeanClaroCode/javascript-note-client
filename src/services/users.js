7e41c403fbba93b5c31ec82d060416f293e2adfe
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-07-18 18:39:38 -0300

    a

--- src/services/users.js
+++ src/services/users.js
@@ -3,9 +3,14 @@ import Api from "./api";
 const UsersService = {
   register: (params) => Api.post("/users/register", params),
   login: async (params) => {
-    const response = await Api.post("/users/login", params);
-    localStorage.setItem("user", JSON.stringify(response.data.user));
-    localStorage.setItem("token", response.data.token);
+    try {
+      const response = await Api.post("/users/login", params);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      localStorage.setItem("token", response.data.token);
+      return response.data.user;
+    } catch (error) {
+      throw new Error("Login failed");
+    }
   },
   logout: () => {
     localStorage.removeItem("user", null);
