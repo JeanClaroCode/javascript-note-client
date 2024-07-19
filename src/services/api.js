454f168b1b65f14a7bea26f76628f8cfb88ce21f
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-07-18 21:03:40 -0300

    Adiciona Procfile e configurações para Heroku com Express

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+const Api = axios.create({
+  baseURL: process.env.REACT_APP_BASE_API || 'https://javascript-notes-api2-44a81e69cf98.herokuapp.com',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
-const Api = axios.create({baseURL:'https://javascript-notes-api2-44a81e69cf98.herokuapp.com'});
-export default Api
+// Adicionando o header 'x-access-token' para todas as requisições
+Api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['x-access-token'] = token;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default Api;
