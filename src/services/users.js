d9f405ac658a99c3d80b2d3310fac0a97c52289f
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-07-21 14:35:35 -0300

    Atualização para deploy

--- src/services/users.js
+++ src/services/users.js
@@ -5,9 +5,13 @@ const UsersService = {
   login: async (params) => {
     try {
       const response = await Api.post('/users/login', params);
-      localStorage.setItem('user', JSON.stringify(response.data.user));
-      localStorage.setItem('token', response.data.token);
-      return response.data.user;
+      if (response.data && response.data.user && response.data.token) {
+        localStorage.setItem('user', JSON.stringify(response.data.user));
+        localStorage.setItem('token', response.data.token);
+        return response.data.user;
+      } else {
+        throw new Error('Invalid response data');
+      }
     } catch (error) {
       console.error('Login failed', error.response ? error.response.data : error.message);
       throw new Error('Login failed');
