a0b3c7faef06b4c0d638c792760579ad096f3410
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-07-21 23:27:21 -0300

    Atualização para deploy

--- src/services/users.js
+++ src/services/users.js
@@ -26,25 +26,32 @@ const UsersService = {
     localStorage.removeItem("token");
   },
   update: async (params) => {
-    const response = await Api.put("/users", params, {
-      headers: { "x-access-token": localStorage.getItem("token") },
-    });
-    localStorage.setItem("user", JSON.stringify(response.data));
-  },
+    try {
+      const response = await Api.put("/users", params, {
+        headers: { "x-access-token": localStorage.getItem("token") },
+      });
+      console.log('User update response:', response.data); // Log para depuração
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      localStorage.setItem("token", response.data.token); // Atualiza o token
+    } catch (error) {
+      console.error('Update failed', error.response ? error.response.data : error.message);
+      throw new Error('Update failed');
+    }
+  },  
   updatePassword: async (params) => {
     const token = localStorage.getItem("token");
-  if (!token) {
-    throw new Error('No token found');
-  }
-
-  try {
-    await Api.put("/users/password", params, {
-      headers: { "x-access-token": token },
-    });
-  } catch (error) {
-    console.error('Update password failed', error.response ? error.response.data : error.message);
-    throw new Error('Update password failed');
-  }
+    if (!token) {
+      throw new Error('No token found');
+    }
+  
+    try {
+      await Api.put("/users/password", params, {
+        headers: { "x-access-token": token },
+      });
+    } catch (error) {
+      console.error('Update password failed', error.response ? error.response.data : error.message);
+      throw new Error('Update password failed');
+    }
   },
   delete: async () => {
     let token = localStorage.getItem("token");
