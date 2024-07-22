d2a553d66c11fd19da3767151da1758d4aa2053e
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-07-21 21:52:05 -0300

    Atualização para deploy

--- src/services/users.js
+++ src/services/users.js
@@ -32,19 +32,9 @@ const UsersService = {
     localStorage.setItem("user", JSON.stringify(response.data));
   },
   updatePassword: async (params) => {
-    const token = localStorage.getItem("token");
-  if (!token) {
-    throw new Error('No token found');
-  }
-
-  try {
     await Api.put("/users/password", params, {
-      headers: { "x-access-token": token },
+      headers: { "x-access-token": localStorage.getItem("token") },
     });
-  } catch (error) {
-    console.error('Update password failed', error.response ? error.response.data : error.message);
-    throw new Error('Update password failed');
-  }
   },
   delete: async () => {
     let token = localStorage.getItem("token");
